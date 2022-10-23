e3b9b4ea6465b6c3c21bbafa68e93c49068a3cb9
guest400123064 <lgfz1003@icloud.com>
2022-10-22 20:19:19 -0400

    docstring

--- keyspacy/__init__.py
+++ keyspacy/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """KeySpaCy is a spaCy integration of KeyBERT by rewriting the embedding backend.
     It is designed to be a component of TRANSFORMER-BASED spacy pipeline, e.g., 
-    <en_core_web_trf> pre-trained pipeline. Unlike KeyBERT, we only use pre-computed
+    `en_core_web_trf` pre-trained pipeline. Unlike KeyBERT, we only use pre-computed
     token contextualized embeddings to measure doc-substring similarities (KeyBERT 
     compute document and keyphrase embeddings independently, resulting in recomputing overhead)."""
 
